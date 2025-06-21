5d8fd99139c52abbaa427a84bcee3afe089fe610
kontrprimer <kontrprimer@gmail.com>
2025-06-21 13:17:12 +0000

    white square

--- game/main.py
+++ game/main.py
@@ -5,15 +5,17 @@ def launch():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
 
-    r = pygame.Rect(50, 50, 100, 200)
-    pygame.draw.rect(screen, (255, 0, 0), r, 5)
+    r = pygame.Rect(350, 250, 100, 100)
+    WHITE = (255, 255, 255)
 
     run = True
     while run:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 run = False
-    # ---Закрытие всех модулей Pygame---
+        pygame.draw.rect(screen, WHITE, r)
+        pygame.display.flip()
+
     pygame.quit()
 
 
