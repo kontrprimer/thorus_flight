87dc91e568ae55e211792e6062bc875a3732303a
kontrprimer <kontrprimer@gmail.com>
2025-06-21 11:26:20 +0000

    initial commit

---
+++ game/main.py
@@ -0,0 +1,6 @@
+def return_2():
+    return 2
+
+
+if __name__ == "__main__":
+    print(return_2())
---
+++ unit_tests/test_main.py
@@ -0,0 +1,5 @@
+from game.main import return_2
+
+
+def test_return_2_returns_2():
+    assert return_2() == 2
