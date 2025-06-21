5dfb1449ea50f4fd6f07511a55684a62139c6702
Dididum <alexandrkaraulchik07@gmail.com>
2025-06-21 16:11:10 +0300

    Square draw

--- game/main.py
+++ game/main.py
@@ -3,7 +3,10 @@ import pygame
 
 def launch():
     pygame.init()
-    pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800, 600))
+
+    r = pygame.Rect(50, 50, 100, 200)
+    pygame.draw.rect(screen, (255, 0, 0), r, 5)
 
     run = True
     while run:
