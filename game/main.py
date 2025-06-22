264c3d21a9c25f183119e5f5298404fb56822b91
Dididum <alexandrkaraulchik07@gmail.com>
2025-06-22 15:29:36 +0300

    The President of Belarus and the Runaway Potato

--- game/main.py
+++ game/main.py
@@ -5,10 +5,12 @@ def launch(true=True):
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
 
+    texture = pygame.image.load("texture_player3.jpg")
+    texture_potato = pygame.image.load("potato.jpg")
     player_position = pygame.Rect(150, 250, 50, 50)
+    potato_position = pygame.Rect(250, 350, 50, 50)
     r = pygame.Rect(350, 250, 100, 100)
     WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
     BLACK = (0, 0, 0)
 
     run = true
@@ -18,24 +20,51 @@ def launch(true=True):
                 run = False
             elif e.type == pygame.KEYDOWN:
                 pygame.draw.rect(screen, BLACK, player_position)
-                act(e.key)
-                player_position = new_position(player_position, act(e.key))
-                print(player_position)
+                last_move = act(e.key)
+                player_position = new_position(player_position, last_move)
+                eat_potato(player_position, potato_position, last_move)
+
+        screen.fill(BLACK)
         pygame.draw.rect(screen, WHITE, r)
-        pygame.draw.rect(screen, RED, player_position)
+
+        for x in range(
+            potato_position.left, potato_position.right, texture.get_width()
+        ):
+            for y in range(
+                potato_position.top, potato_position.bottom, texture.get_height()
+            ):
+                screen.blit(texture_potato, (x, y))
+
+        for x in range(
+            player_position.left, player_position.right, texture.get_width()
+        ):
+            for y in range(
+                player_position.top, player_position.bottom, texture.get_height()
+            ):
+                screen.blit(texture, (x, y))
         pygame.display.flip()
     pygame.quit()
 
 
+def eat_potato(player_position, potato_position, last_move):
+    if player_position == potato_position:
+    if player_position == potato_position:
+        new_position(potato_position, last_move)
+
+
 def new_position(position, command):
     if command == "up":
-        position.move_ip(0, -50)
+        if position.y > 0:
+            position.move_ip(0, -50)
     elif command == "down":
-        position.move_ip(0, 50)
+        if position.y < 550:
+            position.move_ip(0, 50)
     elif command == "left":
-        position.move_ip(-50, 0)
+        if position.x > 0:
+            position.move_ip(-50, 0)
     elif command == "right":
-        position.move_ip(50, 0)
+        if position.x < 750:
+            position.move_ip(50, 0)
     return position
 
 
